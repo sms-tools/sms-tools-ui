c4db8719ad634cba80649226f15cabc65c9cdd06
unel <enee.prog@gmail.com>
2025-02-25 23:03:31 +0100

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import express from 'express';
-import path from 'path';
-import { app } from '../..';
+import { Router } from "express";
+import express from "express";
+import path from "path";
+import { app } from "../..";
 
 function router() {
   const route = Router();
 
-  app.use(express.static(path.join(__dirname, 'dist')));
-  route.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  app.use(express.static(path.join(__dirname, "build")));
+  route.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 
   return route;
